1752fe4595ed068a32c9b192d66a388e67c0fb73
Russell Shea <russell2shea@gmail.com>
2019-02-10 13:23:56 -0500

    pre JSX play

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 // React Tree House Course
+	const title = React.createElement(
+		// type of element, usually a string which represents an html element or tag
+		'h1',
+		//Second arg is an object containint any attributes or value you want to give the element, or null
+		{ id: 'main-title'},
+		// Third arg is any children you want to pass in, or null
+		'Pizza Time!'
+	);
+
+	const desc = React.createElement(
+		'p',
+		null,
+		'Let\'s Eat'
+	);
+
+	const header = React.createElement(
+		'header',
+		null,
+		title, 
+		desc
+	);
+
+
+	ReactDOM.render(
+		header,
+		//Mount into the container ID 
+		document.getElementById('container')
+	);
