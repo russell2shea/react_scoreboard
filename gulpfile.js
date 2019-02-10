5c1884a65b3042d85d0e409cbd07e7ba107b7f38
Russell Shea <russell2shea@gmail.com>
2019-02-10 12:52:38 -0500

    Initial project setup

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+// dependencies
+const gulp         = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+const sass         = require('gulp-sass');
+const browsersync  = require('browser-sync').create();
+const rename       = require('gulp-rename');
+const sourcemaps   = require('gulp-sourcemaps')
+const babel = require('gulp-babel');
+ 
+
+function scss(done){
+  gulp.src('src/components/sass/styles.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({
+            browsers: ['last 4 versions'],
+            cascade: false
+        }))
+  .pipe(gulp.dest('src/components/css/'))
+  .pipe(sass({outputStyle:'compressed'}))
+  .pipe(rename('styles-min.css'))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('css/'))
+  .pipe(browsersync.stream());
+  done();
+}
+
+function browserSync(done) {
+  browsersync.init({
+    server: {
+       baseDir: "./",
+    },
+  });
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch("src/components/sass/*.scss", scss);
+  gulp.watch("*.html", browserSyncReload);
+}
+
+
+gulp.task('default', gulp.parallel(watchFiles, browserSync));
---
+++ js/app.js
@@ -0,0 +1,1 @@
+// React Tree House Course
