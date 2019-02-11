cf05b2ba4bac1b32051a50385b2038d1d13a3d98
Russell Shea <russell2shea@gmail.com>
2019-02-10 19:38:53 -0500

    add state to score Counters

--- js/app.js
+++ js/app.js
@@ -1,35 +1,87 @@
 // React Tree House Course
 	
+	// Array of players
+	const players =[
+	  {
+	    name: "Guil",
+	    score: 50, 
+	    id: 1
+	  },
+	  {
+	    name: "Treasure",
+	    score: 85,
+	    id: 2
+	  },
+	  {
+	    name: "Ashley",
+	    score: 95,
+	    id: 3
+	  },
+	  {
+	    name: "James",
+	    score: 80,
+	    id: 4
+	  }
+	]
+
+
 	// React components are required to have a capital letter to start
 	const Header = (props) => {
 		console.log(props);
 		return (
 			React.createElement("header", null, 
 				React.createElement("h1", null, props.title), 
-				React.createElement("span", {className: "stats"}, "Players: ", props.totalPlayers + 2)
+				React.createElement("span", {className: "stats"}, "Players: ", props.totalPlayers)
 			)
 		);
 	}
 
 	// Create Counters 
-	const Counters = () =>{
-		return (
+	class Counters extends React.Component{
 
-			React.createElement("div", {className: "counter"}, 
-				React.createElement("button", {className: "counter-action decrement"}, " - "), 
-				React.createElement("span", {className: "counter-score"}, "35"), 
-				React.createElement("button", {className: "counter-action increment"}, " + ")
-			)
+		constructor() {
+			super()
+			this.state = {
+				score: 0 
+			};
+		}
 
-		);
+		/* Method to increment score */
+		incrementScore(){
+			//let react know state is changing, and it needs to re-render
+			this.setState({
+				score: this.state.score + 1
+			});
+		}
+
+		/* Method to decrement score */
+		decrementScore(){
+			if(this.state.score > 0){
+				this.setState({
+					score: this.state.score - 1
+				});
+			}
+		}
+
+		render(){
+			return (
+				React.createElement("div", {className: "counter"}, 
+					React.createElement("button", {className: "counter-action decrement", onClick: () => this.decrementScore()}, " - "), 
+					React.createElement("span", {className: "counter-score"}, this.state.score), 
+					React.createElement("button", {className: "counter-action increment", onClick: () => this.incrementScore()}, " + ")
+				)
+
+			);
+		}
+
 	}
 
 	// Create Players 
-	const Players = () =>{
+	const Player = (props) =>{
 		return (
 			React.createElement("div", {className: "player"}, 
 				React.createElement("span", {className: "player-name"}, 
-					"Bob"
+					props.name
 				), 
 
 				React.createElement(Counters, null)
@@ -39,13 +91,26 @@
 	}
 
 	// Compose all components together
-	const App = () => {
+	const App = (props) => {
 		return (
 			React.createElement("div", {className: "scoreboard"}, 
-				React.createElement(Header, {title: "Scoreboard", totalPlayers: 1}), 
+				React.createElement(Header, {
+					title: "Scoreboard", 
+					totalPlayers: props.initialPlayers.length}
+				), 
 
-				/* Players list */
-				React.createElement(Players, null)
+				/* Players list. Interate over players array */
+				props.initialPlayers.map(
+					player => 
+
+					React.createElement(Player, {
+						name: player.name, 
+						key: player.id.toString()}
+					)
+				
+				)
+
+
 			)
 		);
 	}
@@ -53,7 +118,9 @@
 	// Render Root level component to the Dom
 	ReactDOM.render(
 		// Can self close since this has no children
-		React.createElement(App, null),
+		React.createElement(App, {
+			initialPlayers: players}
+		),
 
 		//Mount into the container ID 
 		document.getElementById('container')
--- src/components/js/app.jsx
+++ src/components/js/app.jsx
@@ -1,35 +1,87 @@
 // React Tree House Course
 	
+	// Array of players
+	const players =[
+	  {
+	    name: "Guil",
+	    score: 50, 
+	    id: 1
+	  },
+	  {
+	    name: "Treasure",
+	    score: 85,
+	    id: 2
+	  },
+	  {
+	    name: "Ashley",
+	    score: 95,
+	    id: 3
+	  },
+	  {
+	    name: "James",
+	    score: 80,
+	    id: 4
+	  }
+	]
+
+
 	// React components are required to have a capital letter to start
 	const Header = (props) => {
 		console.log(props);
 		return (
 			<header>
 				<h1>{props.title}</h1>
-				<span className="stats">Players: {props.totalPlayers + 2}</span>
+				<span className="stats">Players: {props.totalPlayers}</span>
 			</header>
 		);
 	}
 
 	// Create Counters 
-	const Counters = () =>{
-		return (
+	class Counters extends React.Component{
 
-			<div className="counter">
-				<button className="counter-action decrement"> - </button>
-				<span className="counter-score">35</span>
-				<button className="counter-action increment"> + </button>
-			</div>
+		constructor() {
+			super()
+			this.state = {
+				score: 0 
+			};
+		}
 
-		);
+		/* Method to increment score */
+		incrementScore(){
+			//let react know state is changing, and it needs to re-render
+			this.setState({
+				score: this.state.score + 1
+			});
+		}
+
+		/* Method to decrement score */
+		decrementScore(){
+			if(this.state.score > 0){
+				this.setState({
+					score: this.state.score - 1
+				});
+			}
+		}
+
+		render(){
+			return (
+				<div className="counter">
+					<button className="counter-action decrement" onClick={() => this.decrementScore()}> - </button>
+					<span className="counter-score">{this.state.score}</span>
+					<button className="counter-action increment" onClick={() => this.incrementScore()}> + </button>
+				</div>
+
+			);
+		}
+
 	}
 
 	// Create Players 
-	const Players = () =>{
+	const Player = (props) =>{
 		return (
 			<div className="player">
 				<span className="player-name">
-					Bob
+					{props.name}
 				</span>
 
 				<Counters />
@@ -39,13 +91,26 @@
 	}
 
 	// Compose all components together
-	const App = () => {
+	const App = (props) => {
 		return (
 			<div className="scoreboard">
-				<Header title="Scoreboard" totalPlayers={1} />
+				<Header 
+					title="Scoreboard" 
+					totalPlayers={props.initialPlayers.length} 
+				/>
 
-				{/* Players list */}
-				<Players />
+				{/* Players list. Interate over players array */}
+				{props.initialPlayers.map(
+					player => 
+
+					<Player 
+						name={player.name}
+						key={player.id.toString()}
+					/>
+				
+				)}
+
+
 			</div>
 		);
 	}
@@ -53,7 +118,9 @@
 	// Render Root level component to the Dom
 	ReactDOM.render(
 		// Can self close since this has no children
-		<App />,
+		<App 
+			initialPlayers={players}
+		/>,
 
 		//Mount into the container ID 
 		document.getElementById('container')
